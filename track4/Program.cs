c29c4f4532e1cf2f79359130a52618fe3472e903
Denis_Zeff <d.sujew@gmail.com>
2022-11-18 00:39:33 +0300

    Track4

---
+++ track4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое число:  " );
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:  " );
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите третье число:  ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int mx = num1;
+
+if(num2>mx)
+{
+ mx = num2;
+}
+if(num3>mx)
+{
+mx = num3;
+}
+
+
+Console.WriteLine( " Максимальное число - " + mx);
+
+
