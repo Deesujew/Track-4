f99ec938773c13fa695dd88e30d65cd3268c6e9b
Denis_Zeff <d.sujew@gmail.com>
2022-11-18 01:38:06 +0300

    Track8

---
+++ track8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число:  " );
+int N = Convert.ToInt32(Console.ReadLine());
+
+int digit = 1;
+
+while (digit < N)
+
+if(digit % 2 ==1)
+{
+ digit++;
+}
+else
+{
+Console.WriteLine( digit  +  " ");   
+digit++;
+}
