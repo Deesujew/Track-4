f72e1b0d51894c5c697d372abed6422a38682b63
Denis_Zeff <d.sujew@gmail.com>
2022-11-18 23:30:02 +0300

    Track23

---
+++ track23/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int number = ReadInt("Введите число: ");
+int i = 1;
+while(i <= number)
+{
+    Console.Write(i*i*i + " ");
+    i++;
+}
+
