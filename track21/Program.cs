26df9a2512c5bfdadf322bee610bbe6260c3f181
Denis_Zeff <d.sujew@gmail.com>
2022-11-18 23:46:06 +0300

    Track21

---
+++ track21/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = ReadInt("Введите координату x точки А: ");
+int y1 = ReadInt("Введите координату y точки А: ");
+int z1 = ReadInt("Введите координату z точки А: ");
+int x2 = ReadInt("Введите координату x точки B: ");
+int y2 = ReadInt("Введите координату x точки B: ");
+int z2 = ReadInt("Введите координату z точки B: ");
+
+
+int x = x2-x1;
+int y = y2-y1;
+int z = z2-z1;
+int dist = (x*x + y*y + z*z);
+double length = Math.Sqrt(dist);
+Console.WriteLine(length);
