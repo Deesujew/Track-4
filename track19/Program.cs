ac2e960e06181963315bf84b03ed06a108e9376e
Denis_Zeff <d.sujew@gmail.com>
2022-11-19 00:15:48 +0300

    Track19

---
+++ track19/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите пятизначное число:  " );
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = num / 10000;
+int b = num / 1000 % 10;
+int g = num / 10 % 10;
+int d = num % 10;
+
+if( a == d && b == g)
+{
+    Console.WriteLine(" палиндром");
+}
+else 
+{
+    Console.WriteLine("не палиндром");
+}
+
