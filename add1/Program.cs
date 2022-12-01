3ca7e26e817732553cc8bb2780ed04b1b5a7bc99
Denis_Zeff <d.sujew@gmail.com>
2022-12-01 17:03:22 +0300

    Add 1

---
+++ add1/Program.cs
@@ -0,0 +1,20 @@
+﻿// В переменной string есть секретное сообщение, во второй есть пароль. 
+// Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+
+string password = "simsim";
+string input;
+string secretText = "Аллилуйя";
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Введите пароль:");
+    input = Console.ReadLine();
+    if (input == password)
+    {
+        Console.WriteLine(secretText);
+        break;
+    }
+}
+
+
+
