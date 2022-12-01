5319262decc9067bd36eb7cc1d59a645e2f123ac
Denis_Zeff <d.sujew@gmail.com>
2022-12-01 15:50:37 +0300

    Additional 2

---
+++ add2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+string input;
+string stop = "exit";
+
+do
+{
+    Console.WriteLine("Введите стоп слово:");
+    input = Console.ReadLine();
+}
+while (input != stop);
