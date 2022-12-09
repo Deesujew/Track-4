a4c422a152ef401347d8abd37f8dd74436b63d87
Denis_Zeff <d.sujew@gmail.com>
2022-12-09 14:58:55 +0300

    Track 47

---
+++ track47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//double rows = 
+
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+Random rnd = new Random();
+
+
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+
+
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+
+
+
+
+
+
