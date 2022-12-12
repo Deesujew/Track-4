140736b0fd9b9e7d051e4c511bf82e064d091bb5
Denis_Zeff <d.sujew@gmail.com>
2022-12-12 16:41:10 +0300

    Track 56

---
+++ track56/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов. Например, задан массив:
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Random rnd = new Random();
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+int minrow =0 ;
+int minsum = int.MaxValue;
+
+
+for (int i = 0; i < m; i++)
+ { 
+    int sum = 0;
+    for (int j = 0; j < n; j++)
+        { 
+            sum += array[i,j];
+        }
+        if ( sum < minsum )
+        {
+            minsum = sum;
+            minrow = i;
+        }
+ }
+
+Console.WriteLine(" Номер строки с наименьшей суммой элементов - " + minrow );
+
+
+
+void FillArray(int[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToInt32(rnd.Next(-20, 20));}}}
+
+
+void PrintArray(int[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
