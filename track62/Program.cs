2aca609d773232ada5d186b3f91cde2f1bd19ec8
Denis_Zeff <d.sujew@gmail.com>
2022-12-13 16:41:22 +0300

    Track 62

---
+++ track62/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4. 
+
+int n = 4;
+int[,] array = new int[n, n];
+
+// Console.WriteLine("Задайте количество строк и столбцов двумерного массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[n, n];
+int t = 1;
+int i = 0;
+int j = 0;
+
+while (t <= array.GetLength(0) * array.GetLength(1))
+{
+  array[i, j] = t;
+  t++;
+  if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= array.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > array.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] matr)
+    {
+        for (int i = 0; i < n; i++)
+            { 
+                for (int j = 0; j < n; j++)
+                    {
+                         Console.Write($"{matr[i, j]} ");
+                    }
+                            Console.WriteLine();
+            }
+    }
