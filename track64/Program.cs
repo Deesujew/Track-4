ac77ea78d75bf31cb24362d2006d9d53ca776e3b
Denis_Zeff <d.sujew@gmail.com>
2022-12-16 15:51:26 +0300

    Track 64

---
+++ track64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int num1 = ReadInt("Введите число M: ");
+//int num2 = ReadInt("Введитt число N: ");
+//Console.WriteLine(NaturalNumber(num1));
+NaturalNumber(num1);
+
+int NaturalNumber(int n)
+{
+    Console.Write(n +", ");
+    if (n == 1)
+        return 0;
+    else
+    {
+        return NaturalNumber(n-1);
+    }
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
